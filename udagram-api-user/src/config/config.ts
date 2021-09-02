18cb20e4388aa44e1e2b030a7406b0b652161b78
ericboss <ericnguifo@gmail.com>
2021-09-02 09:53:24 +0100

    commit code

---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': "postgres",
+  'password': "password",
+  'database': "",
+  'host': "database-1.condsdenctdt.us-east-1.rds.amazonaws.com",
+  'dialect': 'postgres',
+  'aws_region': "us-east-1",
+  'aws_profile': "default",
+  'aws_media_bucket': "test-10299950678-dev",
+  'url': "http://localhost:8100",
+  'jwt': {
+    'secret': "testing",
+  },
+};
